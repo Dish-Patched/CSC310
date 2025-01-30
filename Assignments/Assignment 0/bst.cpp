c93c408a3ae4d4a972f07a176359b7fb50dea066
dishanta <dishanta.sarma@trojans.dsu.edu>
2025-01-29 19:47:27 -0600

    second

---
+++ Assignments/Assignment 0/bst.cpp
@@ -0,0 +1,244 @@
+#include "bst.h"
+
+template<class Type>
+BinarySearchTree<Type>::BinarySearchTree()
+{
+    root = NULL;
+}
+
+template<class Type>
+void BinarySearchTree<Type>::insertNode(const Type x) 
+{
+    Node<Type>* current, *prevCurrent=NULL, *newNode;
+    newNode = new Node<Type>;
+    newNode->info = x;
+    newNode->llink = NULL;
+    newNode->rlink = NULL;
+    if(this->root == NULL){
+        this->root = newNode;
+    }
+    else{
+        current = this->root;
+        while(current != NULL){
+            prevCurrent = current;
+            if(current->info == x){
+                cout << "Node already exists" << endl;
+                return;
+            }
+            else{
+                if(current->info > x)
+                    current = current->llink;
+                else    
+                    current = current->rlink;
+            }
+        }
+        if(prevCurrent->info > x)
+            prevCurrent->llink = newNode;
+        else
+            prevCurrent->rlink = newNode;
+    }
+}
+
+
+template<class Type>
+void BinarySearchTree<Type>::deleteFromTree(Node<Type>*& p)
+{
+	Node<Type>* current, *prevCurrent, *temp;
+	if (p == NULL)
+		cout << "The node to be deleted is NULL!" << endl;
+	else if (p->llink == NULL && p->rlink == NULL) //p points to a leaf
+	{
+		temp = p;
+		p = NULL;
+		delete temp;
+	}
+	else if (p->llink == NULL) //p points to a node with only a right child  
+	{
+		temp = p;
+		p = temp->rlink;
+		delete temp;
+	}
+	else if (p->rlink == NULL) //p points to a node with only a left child 
+	{
+		temp = p;
+		p = temp->llink;
+		delete temp;
+	}
+	else//p points to a node with two children
+	{
+		current = p->llink;
+		prevCurrent = NULL;
+		while (current->rlink != NULL)
+		{
+			prevCurrent = current;
+			current = current->rlink;
+		}
+		p->info = current->info;
+		if (prevCurrent == NULL)   
+			p->llink = current->llink;
+		else
+			prevCurrent->rlink = current->llink;
+		delete current;
+	}
+}
+
+template<class Type>
+void BinarySearchTree<Type>::deleteNode(const Type& x)
+{
+	Node<Type>* current;
+	Node<Type>* prevCurrent;
+	bool found = false;
+	if (this->root == NULL)
+		cout << "Cannot delete from an empty tree!" << endl;
+	else
+	{
+		current = this->root;
+		prevCurrent = this->root;
+		while (current != NULL && !found)
+		{
+			if (current->info == x)
+				found = true;
+			else
+			{
+				prevCurrent = current;
+				if (current->info > x)
+					current = current->llink;
+				else
+					current = current->rlink;
+			}
+		}
+		if (current == NULL)
+			cout << "The item is not in the tree!" << endl;
+		else if (found)
+		{
+			if (current == this->root)
+				deleteFromTree(this->root);
+			else
+				if (prevCurrent->info > x)
+					deleteFromTree(prevCurrent->llink);
+				else
+					deleteFromTree(prevCurrent->rlink);
+		}
+	}
+}
+
+template<class Type>
+bool BinarySearchTree<Type>::search(const Type& x)
+{
+    Node<Type>* current;
+    bool found = false;
+    if(this->root == NULL)
+        cout << "Cannot search in an empty tree" << endl;
+    else{
+        current = this->root;
+        while(current != NULL && !found){
+            if(current->info == x){
+                found = true;
+            }
+            else{
+                if(current->info > x)
+                    current = current->llink;
+                else
+                    current = current->rlink;
+            }
+        }
+    }
+    return found;
+}
+
+template<class Type>
+void BinarySearchTree<Type>::inorderTraversal()
+{
+    return inorder(root);
+}
+
+template<class Type>
+void BinarySearchTree<Type>::preorderTraversal()
+{
+    return preorder(root);
+}
+
+template<class Type>
+void BinarySearchTree<Type>::postorderTraversal()
+{
+    return postorder(root);
+}
+
+template<class Type>
+void BinarySearchTree<Type>::inorder(Node<Type>* x)
+{
+    if(p != NULL){
+        inorder(p->llink);
+        cout << p->info << " ";
+        inorder(p->rlink);
+    }
+}
+
+template<class Type>
+void BinarySearchTree<Type>::preorder(Node<Type>* x)
+{
+    if(p != NULL){
+        cout << p->info << " ";
+        preorder(p->llink);
+        preorder(p->rlink);
+    }
+}
+
+template<class Type>
+void BinarySearchTree<Type>::postorder(Node<Type>* x)
+{
+    if(p != NULL){
+        postorder(p->llink);
+        postorder(p->rlink);
+        cout << p->info << " ";
+    }
+}
+
+template<class Type>
+Type BinarySearchTree<Type>::sum(Node<Type>* p)
+{
+    if(p == NULL)
+        return 0;
+    else{
+        return p->info + sum(p->llink) + sum(p->rlink);
+    }
+}
+
+template<class Type>
+void BinarySearchTree<Type>::treeState(){
+    if(root == NULL)
+        cout << "The tree is empty";
+    else{
+        if(sum(root->llink) == sum(root->rlink))
+            cout << "Balanced Tree";
+        else if(sum(root->llink) > sum(root->rlink))
+            cout << "Left Heavy Tree";
+        else if(sum(root->llink) < sum(root->rlink))
+            cout << "Right Heavy Tree";
+    }
+}
+
+template<class Type>
+Type BinarySearchTree<Type>::max(Type x, Type y)
+{
+    if(x > y)
+        return x;
+    else
+        return y;
+}
+
+template<class Type>
+int BinarySearchTree<Type>::height(Node<Type>* p)
+{
+    if(p == NULL)
+        return 0;
+    else{
+        return 1 + max(height(p->llink), height(p->rlink));
+    }
+}
+
+template<class Type>
+int BinarySearchTree<Type>::treeHeight()
+{
+    return height(root);
+}
